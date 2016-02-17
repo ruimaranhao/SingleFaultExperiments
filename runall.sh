0e98e5e39617cbc54a9ed681576c0890ef128c2f
Rui Maranhao <rui.maranhao@gmail.com>
2016-02-17 09:56:53 -0800

    add all programs

--- runall.sh
+++ runall.sh
@@ -10,3 +10,35 @@ for i in `seq 1 26`; do
   ./gzoltar_diagnose.sh Chart $i TARANTULA Chart-$i-tarantula
   ./gzoltar_diagnose.sh Chart $i NAISH1 Chart-$i-naish1
 done
+
+for i in `seq 1 133`; do
+  echo $i
+  ./gzoltar_diagnose.sh Closure $i OCHIAI Closure-$i-Ochiai
+  ./gzoltar_diagnose.sh Closure $i IDEAL Closure-$i-Ideal
+  ./gzoltar_diagnose.sh Closure $i TARANTULA Closure-$i-tarantula
+  ./gzoltar_diagnose.sh Closure $i NAISH1 Closure-$i-naish1
+done
+
+for i in `seq 1 65`; do
+  echo $i
+  ./gzoltar_diagnose.sh Lang $i OCHIAI Lang-$i-Ochiai
+  ./gzoltar_diagnose.sh Lang $i IDEAL Lang-$i-Ideal
+  ./gzoltar_diagnose.sh Lang $i TARANTULA Lang-$i-tarantula
+  ./gzoltar_diagnose.sh Lang $i NAISH1 Lang-$i-naish1
+done
+
+for i in `seq 1 106`; do
+  echo $i
+  ./gzoltar_diagnose.sh Math $i OCHIAI Math-$i-Ochiai
+  ./gzoltar_diagnose.sh Math $i IDEAL Math-$i-Ideal
+  ./gzoltar_diagnose.sh Math $i TARANTULA Math-$i-tarantula
+  ./gzoltar_diagnose.sh Math $i NAISH1 Math-$i-naish1
+done
+
+for i in `seq 1 27`; do
+  echo $i
+  ./gzoltar_diagnose.sh Time $i OCHIAI Time-$i-Ochiai
+  ./gzoltar_diagnose.sh Time $i IDEAL Time-$i-Ideal
+  ./gzoltar_diagnose.sh Time $i TARANTULA Time-$i-tarantula
+  ./gzoltar_diagnose.sh Time $i NAISH1 Time-$i-naish1
+done
